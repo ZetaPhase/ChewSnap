d7ed8ab5b937634333182e04e82b1985d9bee846
xeliot <daveho8888@gmail.com>
2017-04-07 22:22:40 -0700

    finish meal class

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/Meal.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/Meal.java
@@ -1,8 +1,70 @@
 package io.zetaphase.chewsnap;
 
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
 /**
- * Created by xeliot on 4/7/17.
+ * Created by Dave Ho on 3/7/2017.
  */
 
 public class Meal {
+    private String title;
+    private String description;
+    private String location;
+    private int numStars;
+    private ArrayList<Dish> dishes;
+    private Bitmap image;
+
+    public Meal(String title, String description, String location){
+        this.title = title;
+        this.description = description;
+        this.numStars = 0;
+        this.location = location;
+        dishes = new ArrayList<Dish>();
+    }
+
+    public void setLocation(String location) {this.location = location;}
+
+    public String getLocation() {return this.location;}
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getNumStars(){
+        return numStars;
+    }
+
+    public void setNumStars(int numStars){
+        this.numStars = numStars;
+    }
+
+    public void setDishes(ArrayList<Dish> dishes){
+        this.dishes = dishes;
+    }
+
+    public ArrayList<Dish> getDishes(){
+        return dishes;
+    }
+
+    public void setBitmap(Bitmap image){
+        this.image = image;
+    }
+
+    public Bitmap getBitmap(){
+        return this.image;
+    }
 }
