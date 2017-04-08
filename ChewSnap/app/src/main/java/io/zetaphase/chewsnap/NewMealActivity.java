94fce539c154c238e546e6538edd00332185b3c0
xeliot <daveho8888@gmail.com>
2017-04-07 22:38:47 -0700

    start building new meal activity

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/NewMealActivity.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/NewMealActivity.java
@@ -2,7 +2,14 @@ package io.zetaphase.chewsnap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by xeliot on 4/6/17.
@@ -16,5 +23,64 @@ public class NewMealActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_meal);
+        Button finishButton = (Button) findViewById(R.id.finishButton);
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (MainActivity.dishList.isEmpty()){
+                    Toast.makeText(NewMealActivity.this, "Please add some dishes.", Toast.LENGTH_LONG).show();
+                }else{
+                    boolean a = addMeal();
+                    if(!a){return;}
+                    resetActivity();
+                    Toast.makeText(NewMealActivity.this, "New Meal has been added!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        Button addButton = (Button) findViewById(R.id.addButton);
+        dishListView = (ListView) findViewById(R.id.dishList);
+        MainActivity.dishAdapter = new DishAdapter(this, 0, MainActivity.dishList);
+        dishListView.setAdapter(MainActivity.dishAdapter);
+    }
+
+    public boolean addMeal(){
+
+        TextView title = (TextView) findViewById(R.id.mealName);
+        TextView description = (TextView) findViewById(R.id.description);
+        TextView location = (TextView) findViewById(R.id.restaurantName);
+
+        if(title.getText().toString().isEmpty() || description.getText().toString().isEmpty() || location.getText().toString().isEmpty()){
+            Toast.makeText(this, "Please fill in the remaining fields.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        Meal meal = new Meal(title.getText().toString(), description.getText().toString(), location.getText().toString());
+        meal.setDishes(MainActivity.dishList);
+        double averageRating = 0;
+        for(Dish dish : MainActivity.dishList){
+            averageRating += dish.getRating();
+        }
+        averageRating = (averageRating*1.0)/MainActivity.dishList.size();
+        meal.setNumStars((int) averageRating);
+        int randInt = (int)(Math.random()*MainActivity.dishList.size());
+        meal.setBitmap(MainActivity.dishList.get(randInt).getBitmap());
+        MainActivity.mealList.add(meal);
+        MainActivity.mealAdapter.updateMealList(MainActivity.mealList);
+        MainActivity.mealAdapter.notifyDataSetChanged();
+        Log.d("addMEAL", ""+MainActivity.mealList.get(0).getTitle());
+        return true;
+    }
+
+    public void resetActivity(){
+        MainActivity.dishList = new ArrayList<Dish>();
+        MainActivity.dishAdapter = new DishAdapter(this, 0, MainActivity.dishList);
+        dishListView.setAdapter(MainActivity.dishAdapter);
+        TextView title = (TextView) findViewById(R.id.mealName);
+        TextView description = (TextView) findViewById(R.id.description);
+        TextView restaurant = (TextView) findViewById(R.id.restaurantName);
+        title.setText("");
+        description.setText("");
+        restaurant.setText("");
+
     }
 }
