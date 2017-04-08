bd482656a13ef2a12b1241c78634fbd140c66ef1
xeliot <daveho8888@gmail.com>
2017-04-07 22:23:50 -0700

    finish dish class

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/Dish.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/Dish.java
@@ -1,8 +1,41 @@
 package io.zetaphase.chewsnap;
 
+import android.graphics.Bitmap;
+
 /**
- * Created by xeliot on 4/7/17.
+ * Created by Dave Ho on 3/8/2017.
  */
 
 public class Dish {
+    private Bitmap image;
+    private String title;
+    private String description;
+    private int rating;
+
+    public Dish(String title, String description, int rating){
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getRating(){
+        return rating;
+    }
+
+    public void setBitmap(Bitmap image){
+        this.image = image;
+    }
+
+    public Bitmap getBitmap(){
+        return this.image;
+    }
+
 }
