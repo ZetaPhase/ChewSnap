c325b48cbb3d4e3ad4f55c52b14bbedd6171c18f
xeliot <daveho8888@gmail.com>
2017-04-08 21:07:45 -0700

    finish popdish class

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/PopDish.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/PopDish.java
@@ -1,8 +1,247 @@
 package io.zetaphase.chewsnap;
 
-/**
- * Created by xeliot on 4/8/17.
- */
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.text.format.Time;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PopDish {
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Dave Ho on 3/8/2017.
+ */
+public class PopDish extends Activity{
+
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private Uri mHighQualityImageUri = null;
+    private ImageView dishImage;
+    private String userChoosenTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.new_dish_popup);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*0.8), (int) (height*0.8));
+
+        Button finishDishButton = (Button) findViewById(R.id.finishDishButton);
+        Button addImageButton = (Button) findViewById(R.id.addDishImage);
+        dishImage = (ImageView) findViewById(R.id.dishImage);
+        finishDishButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextView title = (TextView) findViewById(R.id.dishName);
+                TextView description = (TextView) findViewById(R.id.dishDescription);
+                RatingBar rating = (RatingBar) findViewById(R.id.dishRating);
+                ImageView image = (ImageView) findViewById(R.id.dishImage);
+                Bitmap bitmap = null;
+                try{
+                    bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+                    Matrix matrix = new Matrix();
+                    matrix.postRotate(90);
+                    Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                    bitmap = rotatedBitmap;
+                }catch(java.lang.NullPointerException e){
+                    Toast.makeText(PopDish.this, "Please select an image.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(title.getText().toString().isEmpty() || description.getText().toString().isEmpty() || (int)rating.getRating()==0){
+                    Toast.makeText(PopDish.this, "Please fill in the remaining fields.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Dish dish = new Dish(title.getText().toString(), description.getText().toString(), (int) rating.getRating());
+                dish.setBitmap(bitmap);
+
+                MainActivity.dishList.add(dish);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        MainActivity.dishAdapter.updateDishList(MainActivity.dishList);
+                        MainActivity.dishAdapter.notifyDataSetChanged();
+                    }
+                });
+                finish();
+            }
+        });
+        addImageButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Log.d("ADDIMAGE", "add image clicked");
+                selectImage();
+            }
+        });
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(PopDish.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result=Utility.checkPermission(PopDish.this);
+
+                if (items[item].equals("Take Photo")) {
+                    userChoosenTask ="Take Photo";
+                    if(result)
+                        cameraIntent();
+
+                } else if (items[item].equals("Choose from Library")) {
+                    userChoosenTask ="Choose from Library";
+                    if(result)
+                        galleryIntent();
+
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(userChoosenTask.equals("Take Photo"))
+                        cameraIntent();
+                    else if(userChoosenTask.equals("Choose from Library"))
+                        galleryIntent();
+                } else {
+                    //code for deny
+                }
+                break;
+        }
+    }
+
+    private void galleryIntent()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
+    }
+
+    private void cameraIntent()
+    {
+        mHighQualityImageUri = generateTimeStampPhotoFileUri();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mHighQualityImageUri);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    private Uri generateTimeStampPhotoFileUri() {
+
+        Uri photoFileUri = null;
+        File outputDir = getPhotoDirectory();
+        if (outputDir != null) {
+            Time t = new Time();
+            t.setToNow();
+            File photoFile = new File(outputDir, System.currentTimeMillis()
+                    + ".jpg");
+            //photoFileUri = Uri.fromFile(photoFile);
+            photoFileUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", photoFile);
+        }
+        return photoFileUri;
+    }
+
+    private File getPhotoDirectory() {
+        File outputDir = null;
+        String externalStorageStagte = Environment.getExternalStorageState();
+        if (externalStorageStagte.equals(Environment.MEDIA_MOUNTED)) {
+            File photoDir = Environment
+                    .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            outputDir = new File(photoDir, getString(R.string.app_name));
+            if (!outputDir.exists())
+                if (!outputDir.mkdirs()) {
+                    Toast.makeText(
+                            this,
+                            "Failed to create directory "
+                                    + outputDir.getAbsolutePath(),
+                            Toast.LENGTH_SHORT).show();
+                    outputDir = null;
+                }
+        }
+        return outputDir;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        /*
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+        //dishImage.setImageBitmap(thumbnail);
+        dishImage.setImageURI(mHighQualityImageUri);
+        dishImage.setRotation(90);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm=null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        dishImage.setImageBitmap(bm);
+        dishImage.setRotation(90);
+    }
 }
