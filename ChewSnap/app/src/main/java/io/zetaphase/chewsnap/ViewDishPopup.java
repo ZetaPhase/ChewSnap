8da483ec69e799c55313154a973b0d202e52c754
xeliot <daveho8888@gmail.com>
2017-04-13 21:26:57 -0700

    finish viewdish popup class

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/ViewDishPopup.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/ViewDishPopup.java
@@ -8,9 +8,36 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-/**
- * Created by xeliot on 4/13/17.
- */
+public class ViewDishPopup extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
 
-public class ViewDishPopup extends Activity {
+        setContentView(R.layout.view_dish_popup);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*0.8), (int) (height*0.8));
+
+        Intent intent = getIntent();
+        int clickedPosition = Integer.valueOf(intent.getStringExtra("CLICKPOSITION"));
+
+        Dish dish = MainActivity.dishList.get(clickedPosition);
+
+        TextView title = (TextView) findViewById(R.id.viewDishTitle);
+        TextView description = (TextView) findViewById(R.id.viewDishDescription);
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.viewDishRating);
+        ImageView image = (ImageView) findViewById(R.id.viewDishImage);
+
+        title.setText(dish.getTitle());
+        description.setText(dish.getDescription());
+        ratingBar.setRating(dish.getRating());
+        image.setImageBitmap(dish.getBitmap());
+
+
+    }
 }
