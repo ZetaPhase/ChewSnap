f618d57a4a798180f3d606d1af8bbf6323cbfe02
xeliot <daveho8888@gmail.com>
2017-04-15 21:07:08 -0700

    This is new activity

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/InboxActivity.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/InboxActivity.java
@@ -1,8 +1,22 @@
 package io.zetaphase.chewsnap;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ListView;
+
 /**
  * Created by xeliot on 4/14/17.
  */
 
-public class InboxActivity {
+public class InboxActivity extends Activity {
+
+    private ListView inboxListView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
 }
